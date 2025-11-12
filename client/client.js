6d7171edde67ca8023d0fa7e1c504853bb1523ae
uiyoung <eyseo@fermi.co.kr>
2025-11-12 15:36:25 +0900

    feat: add joined message

--- client/client.js
+++ client/client.js
@@ -1,8 +1,22 @@
 const socket = io();
 
-let roomId;
+let peerConnection = null;
 let localStream = null;
-let peerConnection;
+let roomId = null;
+
+const rtcConfig = {
+  // ICE 서버 설정(Google 공개 STUN 서버 사용)
+  iceServers: [
+    {
+      urls: [
+        'stun:stun.l.google.com:19302',
+        'stun:stun1.l.google.com:19302',
+        'stun:stun2.l.google.com:19302',
+        'stun:stun3.l.google.com:19302',
+      ],
+    },
+  ],
+};
 
 const localVideo = document.querySelector('#localVideo');
 const remoteVideo = document.querySelector('#remoteVideo');
@@ -12,7 +26,9 @@ const mediaToggleButton = document.querySelector('#mediaToggleButton');
 const joinButton = document.querySelector('#joinButton');
 const leaveButton = document.querySelector('#leaveButton');
 const roomInfoText = document.querySelector('#roomInfo');
+const videoContainer = document.querySelector('#videoContainer');
 
+// 미디어스트림 가져오기(camera, mic)
 async function getMedia() {
   try {
     localStream = await navigator.mediaDevices.getUserMedia({
@@ -42,47 +58,58 @@ function disableMedia() {
 
 // PeerConnection을 생성하고 ICE candidate를 처리하는 함수
 function createPeerConnection() {
-  const configuration = {
-    iceServers: [
-      {
-        urls: [
-          'stun:stun.l.google.com:19302',
-          'stun:stun1.l.google.com:19302',
-          'stun:stun2.l.google.com:19302',
-          'stun:stun3.l.google.com:19302',
-        ],
-      },
-    ],
-  };
-  peerConnection = new RTCPeerConnection(configuration);
+  peerConnection = new RTCPeerConnection(rtcConfig);
 
-  peerConnection.onicecandidate = (event) => {
-    if (event.candidate) {
-      socket.emit('candidate', { roomId, candidate: event.candidate });
-    }
-  };
-
-  peerConnection.ontrack = (event) => {
-    remoteVideo.srcObject = event.streams[0];
-  };
-
-  // stream 추가
+  // 로컬 트랙을 RTCPeerConnection에 추가하여 상대방에게 전송 준비
   localStream.getTracks().forEach((track) => {
     peerConnection.addTrack(track, localStream);
   });
+
+  // 상대방의 ICE Candidate 수신 시 socket.io로 전송
+  peerConnection.addEventListener('icecandidate', (e) => {
+    if (e.candidate) {
+      // 서버를 통해 상대방에게 ICE 후보 정보를 전송
+      socket.emit('candidate', { roomId, candidate: e.candidate });
+    }
+  });
+
+  // 상대방으로부터 스트림(트랙) 수신 시 <video>에 연결
+  peerConnection.addEventListener('track', (e) => {
+    remoteVideo.srcObject = e.streams[0];
+  });
+
+  // 연결 상태 로깅
+  peerConnection.addEventListener('connectionstatechange', () => {
+    console.log(`Connection: ${peerConnection.connectionState}`);
+  });
 }
 
-function initCall() {
-  createPeerConnection();
-  socket.emit('join', roomId);
+function cleanup() {
+  // readyButton.disabled = false;
+  roomIdInput.disabled = false;
+  joinButton.disabled = false;
+  leaveButton.disabled = true;
+
+  // if (peerConnection) {
+  //   peerConnection.getSenders().forEach(s => s.track && s.track.stop());
+  //   peerConnection.close();
+  //   peerConnection = null;
+  // }
+  // if (localStream) {
+  //   localStream.getTracks().forEach((track) => track.stop());
+  //   localStream = null;
+  //   localVideo.srcObject = null;
+  // }
+  // remoteVideo.srcObject = null;
 }
 
 readyButton.addEventListener('click', async () => {
   try {
     await getMedia();
+    roomIdInput.disabled = false;
     readyButton.disabled = true;
     joinButton.disabled = false;
-    document.querySelector('#videoContainer').style.display = 'flex';
+    videoContainer.style.display = 'flex';
   } catch (error) {
     alert('카메라나 마이크를 사용할 수 없습니다. 브라우저 설정을 확인해주세요.');
   }
@@ -96,14 +123,21 @@ joinButton.addEventListener('click', () => {
     return;
   }
 
+  createPeerConnection();
+
+  // socket.io 서버에 방 참가 요청
+  socket.emit('join', roomId);
+
   joinButton.disabled = true;
   roomIdInput.disabled = true;
-  roomInfoText.textContent = `room ${roomId} joined`;
-
-  initCall();
 });
 
-mediaToggleButton.addEventListener('click', async () => {
+// TODO: 
+leaveButton.addEventListener('click', () => {
+
+});
+
+mediaToggleButton.addEventListener('click', () => {
   mediaToggleButton.classList.toggle('on');
   const isCurrentlyOn = mediaToggleButton.classList.contains('on');
 
@@ -116,17 +150,30 @@ mediaToggleButton.addEventListener('click', async () => {
   }
 });
 
-// 방에 참가한 경우, 로컬 비디오 스트림을 추가하고 offer를 생성
-socket.on('welcome', async () => {
-  console.log('receive welcome');
+/* socket.io 시그널링 수신 처리 */
+// 방 참가가 되었음을 알림
+socket.on('joined', ({ roomId }) => {
+  roomInfoText.textContent = `joined room #${roomId}`;
+  leaveButton.disabled = false;
+});
 
+// 방이 full임을 알림
+socket.on('room-full', ({roomId, clientCount}) =>{
+  cleanup();
+  roomInfoText.textContent = `room ${roomId} is full`
+})
+
+// 방에 두 번째 사용자가 들어와 통화 준비가 되었을 때
+socket.on('peer-joined', async () => {
+  // offer를 생성하고 전송(첫번째 사용자 역할)
   const offer = await peerConnection.createOffer();
   await peerConnection.setLocalDescription(offer);
 
   socket.emit('offer', { roomId, offer });
+  console.log('offer 전송 완료')
 });
 
-// 상대방이 offer를 수신한 경우, answer를 생성
+// 상대방이 보낸 offer를 수신한 경우, answer를 생성
 socket.on('offer', async (offer) => {
   console.log('receive offer');
 
@@ -138,7 +185,7 @@ socket.on('offer', async (offer) => {
   socket.emit('answer', { roomId, answer });
 });
 
-// 상대방이 answer를 수신한 경우, remoteDescription을 설정
+// 상대방이 보낸 answer를 수신한 경우, remoteDescription을 설정
 socket.on('answer', async (answer) => {
   console.log('receive answer');
   await peerConnection.setRemoteDescription(answer);
--- server/server.js
+++ server/server.js
@@ -15,16 +15,30 @@ const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, '../client')));
 
+const MAX_CLIENTS_NUM = 2;
+
 io.on('connection', (socket) => {
-  console.log('new client connected', socket.id);
+  console.log(`new client connected ${socket.id}`);
 
+  // Join Room
   socket.on('join', (roomId) => {
-    console.log(`client ${socket.id} joined room: ${roomId}`);
-    socket.join(roomId);
+    const clientCount = io.sockets.adapter.rooms.get(roomId)?.size;
+    if (clientCount >= MAX_CLIENTS_NUM) {
+      socket.emit('room-full', { roomId, clientCount });
+      return;
+    }
 
-    socket.to(roomId).emit('welcome');
+    socket.join(roomId);
+    console.log(`${socket.id} joined room: ${roomId}`);
+
+    // 방 참가가 정상 처리됨을 본인에게 전달
+    socket.emit('joined', { roomId });
+
+    // 기존 참가자에게 새 참가자를 알림 
+    socket.to(roomId).emit('peer-joined', { userId: socket.id });
   });
 
+  // WebRTC Offer/Answer/ICE Candidate 중계
   socket.on('offer', ({ roomId, offer }) => {
     console.log('offer received', roomId);
     socket.to(roomId).emit('offer', offer);
@@ -40,6 +54,7 @@ io.on('connection', (socket) => {
     socket.to(roomId).emit('candidate', candidate);
   });
 
+  // 연결 해제
   socket.on('disconnect', () => {
     console.log('client disconnected', socket.id);
   });
